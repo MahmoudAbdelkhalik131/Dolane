0eaae2dd6624737f5a6e93bbd3c8dd832b3a6cdd
Mahmoud <mahmoudabdelkhalik131@gmail.com>
2025-10-09 07:48:58 +0300

    just adding a new seasion with different expire date

--- src/middleware/Jwt.ts
+++ src/middleware/Jwt.ts
@@ -8,6 +8,13 @@ class JWT{
 verifyToken =(token:string)=>{
    return jwt.verify(token,process.env.JWT_SECRET!)
 }
+ createTokenCode =(user:User)=>{
+    const expire:any=process.env.JWT_EXPIRES_IN_CODE!;
+    return jwt.sign({user},process.env.JWT_SECRET!,{expiresIn: expire})
+}
+verifyTokenCode =(token:string)=>{
+   return jwt.verify(token,process.env.JWT_SECRET!)
+}
 }
 const Jwt =new JWT();
 export default Jwt;
